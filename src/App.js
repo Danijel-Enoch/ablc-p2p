62e89e1d808d6baf93aa489aa633f18c67390ca2
Danijel-Enoch <enochdanijel@gmail.com>
2023-01-11 01:29:24 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/navbar';
+// import back from "./components/back.jpg";
+import P2P from './components/P2P';
+function App() {
+  return (
+    <div className="App">
+      {/* <Nav/>
+      <img src={back}></img> */}
+      <P2P/>
+    </div>
+  );
+}
+
+export default App;
