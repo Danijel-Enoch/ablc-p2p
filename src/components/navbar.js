690a28016892d828cd12b068319584e8274ff314
Danijel-Enoch <enochdanijel@gmail.com>
2023-01-11 01:30:17 +0100

    init commit

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function Nav(){
+    return (
+        <>
+        <ul className="navstyle">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        </>
+    )
+}
+export default Nav;
